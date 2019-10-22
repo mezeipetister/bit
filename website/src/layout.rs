dfc1be7645a2b7121c6341e8a468735a4eaea4b0
Peter Mezei <mezeipetister@gmail.com>
2019-10-22 13:10:24 +0200

    layout small refact

--- website/src/layout.rs
+++ website/src/layout.rs
@@ -59,18 +59,18 @@ impl<'a> Layout<'a> {
             html {
                 head {
                     meta charset="utf-8" /
-                    meta name="viewport" content="width=device-width, initial-scale=1" /
+                    meta name="viewport" content="width=device-width, initial-scale=1";
                     title {(self.title.unwrap_or("TITLE"))}
-                    link rel="stylesheet" type="text/css" href="/static/style.css" /
-                    link rel="icon" type="image/x-icon" href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=" /
+                    link rel="stylesheet" type="text/css" href="/static/style.css";
+                    link rel="icon" type="image/x-icon" href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=";
                 }
                 body {
                     (self.navbar.as_ref().unwrap_or(&html!{}))
                     (self.tabbar.as_ref().unwrap_or(&html!{}))
                     (body)
                     (self.footer.as_ref().unwrap_or(&html!{}))
-                    script defer? src="/static/fa.js" {}
                     script defer? src="/static/script.js" {}
+                    script defer? src="/static/fa.js" {}
                 }
             }
         }
