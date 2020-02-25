95e88fc6ed17c3324fe236f5a618b3994688d060
Peter <mezeipetister@gmail.com>
2020-02-25 17:48:04 +0100

    ui done for the first version

--- client/src/app/admin/dashboard/dashboard.component.ts
+++ client/src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartDataSets } from 'chart.js';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 
 @Component({
@@ -11,14 +11,43 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+
+  barChartData: ChartDataSets[] = [
+    { data: [15132000, 16000000, 16500000, 5000000], label: 'Beruházások' }
+  ];
+
   lineChartData: ChartDataSets[] = [
-    { data: [100, 110, 90, 120, 121, 122, 140, 150, 157], label: 'Pénzeszköz alakulása' },
+    { data: [100, 110, 90, 120, 121, 122, 140, 150, 157], label: 'Pénzeszköz HUF' },
   ];
 
   lineChartLabels: Label[] = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
 
-  lineChartOptions = {
+  ChartOptions = {
     responsive: true,
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItems, data) {
+          return data.labels[tooltipItems.index] + '' + data.datasets[0].data[tooltipItems.index].toLocaleString();
+        }
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false,
+          callback: function (value, index, values) {
+            return value.toLocaleString();
+          }
+        }
+      }]
+    }
   };
 
   lineChartColors: Color[] = [
