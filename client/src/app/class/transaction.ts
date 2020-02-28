dfe094327b9663877a38d3856f71ba661f98b7a2
Peter <mezeipetister@gmail.com>
2020-02-28 15:49:45 +0100

    ..

--- client/src/app/class/transaction.ts
+++ client/src/app/class/transaction.ts
@@ -16,7 +16,7 @@ export class TransactionNew {
         public subject: string = "",
         public debit: string = "",
         public credit: string = "",
-        public amount: number = null,
+        public amount: number = 0,
         public date_settlement: string = new Date().toISOString().split('T')[0],
     ) { }
 }
