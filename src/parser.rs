9aa1bf7b8cc8c262099266df7d389efc67b7f502
Peter Mezei <mezeipetister@gmail.com>
2021-05-04 12:16:36 +0200

    ..

--- src/parser.rs
+++ src/parser.rs
@@ -191,6 +191,7 @@ pub enum Expression {
   Event(EventExp),
 }
 
+#[derive(Debug)]
 pub enum ModeExp {
   Account,
   Balance,
@@ -202,7 +203,7 @@ impl Parser for ModeExp {
   fn parse_params(params: &Vec<(String, String)>) -> Result<Self, String> {
     let first = &params[0];
     match first.0.as_str() {
-      "set" => match first.1.as_str() {
+      "set" | "SET" => match first.1.as_str() {
         "account" => Ok(ModeExp::Account),
         "balance" => Ok(ModeExp::Balance),
         "profit" => Ok(ModeExp::Profit),
@@ -214,8 +215,10 @@ impl Parser for ModeExp {
   }
 }
 
+#[derive(Debug)]
 pub struct CommentExp(String);
 
+#[derive(Debug)]
 pub struct AccountExp {
   id: String,
   name: String,
@@ -227,8 +230,8 @@ impl Parser for AccountExp {
     let mut name: Option<String> = None;
     for row in from {
       match row.0.as_str() {
-        x if x == "id" => id = Some(x.to_string()),
-        x if x == "name" => name = Some(x.to_string()),
+        "id" | "ID" => id = Some(row.1.to_string()),
+        "name" | "NAME" => name = Some(row.1.to_string()),
         _ => return Err("Unknown parameter".to_string()),
       }
     }
@@ -242,31 +245,59 @@ impl Parser for AccountExp {
 pub struct TransactionExp {
   debit: String,
   credit: String,
-  event_id: Option<Uuid>,
+  event_id: Option<u32>,
   cdate: Option<NaiveDate>,
   amount: i32,
 }
 
-// impl Parser for TransactionExp {
-//   fn parse(from: &Vec<(String, String)>) -> Result<Self, String> {
-//     let mut debit: Option<String> = None;
-//     let mut credit: Option<String> = None;
-//     let mut event_id: Option<String> = None;
-//     let mut cdate: Option<String> = None;
-//     let mut amount: Option<String> = None;
-//     for row in from {
-//       match row.0.as_str() {
-//         x if x == "id" => id = Some(x.to_string()),
-//         x if x == "name" => name = Some(x.to_string()),
-//         _ => return Err("Unknown parameter".to_string()),
-//       }
-//     }
-//     Ok(Self {
-//       id: id.ok_or("No ID given".to_string())?,
-//       name: name.ok_or("No NAME given".to_string())?,
-//     })
-//   }
-// }
+impl Parser for TransactionExp {
+  fn parse_params(from: &Vec<(String, String)>) -> Result<Self, String> {
+    let mut debit: Option<String> = None;
+    let mut credit: Option<String> = None;
+    let mut event_id: Option<u32> = None;
+    let mut cdate: Option<NaiveDate> = None;
+    let mut amount: Option<i32> = None;
+    for row in from {
+      match row.0.as_str() {
+        "debit" | "DEBIT" => debit = Some(row.1.to_string()),
+        "credit" | "CREDIT" => credit = Some(row.1.to_string()),
+        "event_id" | "EVENT_ID" => {
+          event_id = Some(
+            row
+              .1
+              .parse::<u32>()
+              .map_err(|_| "Event ID must be positive integer")?,
+          )
+        }
+        "cdate" | "CDATE" => {
+          cdate = Some(
+            row
+              .1
+              .parse::<NaiveDate>()
+              .map_err(|_| "Wrong date format")?,
+          )
+        }
+        "amount" | "AMOUNT" => {
+          amount = Some(
+            row
+              .1
+              .replace("_", "")
+              .parse::<i32>()
+              .map_err(|_| "Amount must be integer number")?,
+          )
+        }
+        _ => return Err("Unknown parameter".to_string()),
+      }
+    }
+    Ok(Self {
+      debit: debit.ok_or("No debit given")?,
+      credit: credit.ok_or("No credit given")?,
+      event_id,
+      cdate,
+      amount: amount.ok_or("No amount given")?,
+    })
+  }
+}
 
 pub struct ReferenceExp {
   id: String,
@@ -379,5 +410,31 @@ mod tests {
   #[test]
   fn test_param_parse() {
     let token_stream = "NAME lorem AGE 32 IMPORTANT";
+  }
+
+  #[test]
+  fn test_exp_mode() {
+    let params = vec![("SET".to_string(), "account".to_string())];
+    assert_eq!(ModeExp::parse_params(&params).is_ok(), true);
+  }
+
+  #[test]
+  fn test_exp_account() {
+    let params = vec![
+      ("ID".to_string(), "161".to_string()),
+      ("NAME".to_string(), "lorem ipsum".to_string()),
+    ];
+    assert_eq!(AccountExp::parse_params(&params).is_ok(), true);
+  }
+
+  #[test]
+  fn test_exp_transaction() {
+    let params = vec![
+      ("debit".to_string(), "161".to_string()),
+      ("credit".to_string(), "38".to_string()),
+      ("amount".to_string(), "40000".to_string()),
+      ("amount".to_string(), "40_000".to_string()),
+    ];
+    assert_eq!(TransactionExp::parse_params(&params).is_ok(), true);
   }
 }
