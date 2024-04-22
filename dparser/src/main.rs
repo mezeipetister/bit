864f731cd827542f58b0186f5a87475cdc6dd94c
Peter Mezei <mezeipetister@gmail.com>
2024-04-22 08:19:59 +0200

    ..

--- dparser/src/main.rs
+++ dparser/src/main.rs
@@ -1,115 +1,135 @@
-use std::{
-    fs::File,
-    io::{Read, Write},
-    path::Path,
-};
-
-use chrono::{NaiveDate, Utc};
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-enum Kind {
-    Note(NoteObj),
-    Account(Account),
+#[derive(Debug)]
+enum MatchResult {
+    CommandMatch(Vec<String>, fn(&str) -> Result<String, String>),
+    CommandSuggestion(String),
+    PathMatch(String),
+    PathSuggestion(String),
+    None,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct NoteObj {
-    id: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    cdate: Option<NaiveDate>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    ddate: Option<NaiveDate>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    idate: Option<NaiveDate>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    net: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    vat: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    gross: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    comment: Option<String>,
-    transactions: Vec<Transaction>,
+struct Command {
+    path: &'static str,
+    help: &'static str,
+    fn_ptr: fn(&str) -> Result<String, String>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Transaction {
-    c: String,
-    d: String,
-    amount: f32,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    comment: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    cdate: Option<String>,
-}
+impl Command {
+    fn new(
+        path: &'static str,
+        help: &'static str,
+        fn_ptr: fn(&str) -> Result<String, String>,
+    ) -> Self {
+        Self { path, fn_ptr, help }
+    }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Account {
-    id: String,
-    name: String,
-}
+    fn match_path(&self, path: &str) -> MatchResult {
+        let command_tokens = tokenize_line(self.path);
+        let input_tokens = tokenize_line(path);
+        // Check complete command match
+        if input_tokens.starts_with(&command_tokens) {
+            return MatchResult::CommandMatch(
+                input_tokens[command_tokens.len()..].to_vec(),
+                self.fn_ptr,
+            );
+        }
 
-fn create_account(id: &str, name: &str) -> Account {
-    Account {
-        id: id.into(),
-        name: name.into(),
+        // Suggesting command
+        let (command_path, command_cmd) = split_last_token(&command_tokens);
+        let (input_path, input_cmd) = split_last_token(&input_tokens);
+
+        if command_path == input_path {
+            if let Some(cmd) = command_cmd {
+                if let Some(_cmd) = input_cmd {
+                    if cmd.starts_with(_cmd) {
+                        return MatchResult::CommandSuggestion(cmd.to_string());
+                    }
+                }
+            }
+        }
+
+        // Path match
+        if command_path == input_tokens {
+            return MatchResult::PathMatch(path.to_string());
+        }
+
+        // Path suggestion
+        if self.path.starts_with(&path) {
+            if let Some(p) = command_path.last() {
+                return MatchResult::PathSuggestion(p.to_string());
+            }
+        }
+
+        MatchResult::None
     }
 }
 
-fn create_note() -> NoteObj {
-    NoteObj {
-        id: "2024-ab-0001".into(),
-        cdate: Utc::now().date_naive().into(),
-        ddate: Utc::now().date_naive().into(),
-        idate: Utc::now().date_naive().into(),
-        net: Some(100.0),
-        vat: Some(27.0),
-        gross: Some(127.0),
-        comment: None,
-        transactions: vec![
-            Transaction {
-                c: "1".into(),
-                d: "2".into(),
-                amount: 100.0,
-                comment: Some("This is a sample comment".into()),
-                cdate: None,
-            },
-            Transaction {
-                c: "1".into(),
-                d: "2".into(),
-                amount: 27.0,
-                comment: Some("This is a sample comment".into()),
-                cdate: None,
-            },
-        ],
+fn split_last_token(tokens: &Vec<String>) -> (Vec<&str>, Option<&String>) {
+    if tokens.is_empty() {
+        return (vec![], None);
     }
+
+    if tokens.len() > 1 {
+        let last = tokens.last().unwrap();
+        let rest = &tokens[..tokens.len() - 1];
+        return (rest.iter().map(|s| s.as_str()).collect(), Some(last));
+    }
+
+    (tokens.iter().map(|s| s.as_str()).collect(), None)
 }
 
-fn write() {
-    let n = create_note();
-    let s = serde_yaml::to_string(&n).unwrap();
-    let mut f = std::fs::File::create("demo_note.txt").unwrap();
-    f.write_all(s.as_bytes()).unwrap();
-}
+fn tokenize_line(line: &str) -> Vec<String> {
+    let mut tokens = vec![];
+    let mut current_token = String::new();
+    let mut inside_quotes = false;
 
-fn write_list(to: &str) {
-    let mut f = File::create(to).unwrap();
-    let n = vec![
-        Kind::Account(create_account("1", "Assets")),
-        Kind::Account(create_account("2", "Demo")),
-        Kind::Account(create_account("3", "Wowow")),
-    ];
-    serde_yaml::to_writer(f, &n).unwrap();
-}
+    for c in line.chars() {
+        if c == '"' {
+            inside_quotes = !inside_quotes;
+            current_token.push(c);
+        } else if c == '/' && !inside_quotes {
+            if !current_token.is_empty() {
+                tokens.push(current_token.clone());
+                current_token.clear();
+            }
+        } else if c.is_whitespace() && !inside_quotes {
+            if !current_token.is_empty() {
+                tokens.push(current_token.clone());
+                current_token.clear();
+            }
+        } else {
+            current_token.push(c);
+        }
+    }
 
-fn read() -> NoteObj {
-    let mut f = File::open("demo/d.yaml").unwrap();
-    let mut c = String::new();
-    f.read_to_string(&mut c).unwrap();
-    serde_yaml::from_str(&c).unwrap()
+    if !current_token.is_empty() {
+        tokens.push(current_token);
+    }
+
+    tokens
 }
 
 fn main() {
-    write_list("demo/list.yaml");
+    let cmd = Command::new(
+        "/settings/user print",
+        "Print user settings lorem ipsum dolorem set ami",
+        |_| Err("".to_string()),
+    );
+
+    println!("{:?}", cmd.match_path("/settings/user print"));
+    println!("{:?}", cmd.match_path("/settings/user print all"));
+    println!("{:?}", cmd.match_path("/settings/user print all params"));
+    println!("{:?}", cmd.match_path("/settings/user print all a=1 b=2"));
+    println!("{:?}", cmd.match_path("/settings/user pri"));
+    println!("{:?}", cmd.match_path("/settings/user"));
+    println!("{:?}", cmd.match_path("/settings/us"));
+
+    let cmd = Command::new("/note create", "Create a new note", |_| Err("".to_string()));
+
+    println!("{:?}", cmd.match_path("/note create new"));
+    println!("{:?}", cmd.match_path("/note create"));
+    println!("{:?}", cmd.match_path("/note cr"));
+    println!("{:?}", cmd.match_path("/note"));
+    println!("{:?}", cmd.match_path("/no"));
+
+    // println!("{:?}", &r);
 }
